508991e3df417a6af6f1aeb1391359e17bbf6f8b
Michael Chow <machow@princeton.edu>
2017-07-04 11:17:23 +0200

    update purrr version

--- requirements.r
+++ requirements.r
@@ -1,3 +1,3 @@
 devtools::install_version('dplyr', '0.7.1')
-devtools::install_version('purrr', '0.2.2')
+devtools::install_version('purrr', '0.2.2.2')
 devtools::install_github('jennybc/repurrrsive')
